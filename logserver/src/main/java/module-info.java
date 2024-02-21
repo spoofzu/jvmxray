cc0aff4aee940ec54f6b3e28a848c8a33058cbc9
Milton Smith <milton.smith.rr@gmail.com>
2024-02-21 14:25:07 -0600

    add modularity

---
+++ logserver/src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module jvmxray.logserver {
+    // Project Dependencies
+    requires jvmxray.shared;
+
+    // Project External Dependencies
+    requires ch.qos.logback.classic;
+    requires commons.cli;
+    requires org.slf4j;
+
+    // Private Project Exports
+    // None
+
+    // Public API Exports
+    // None
+}
---
+++ shared/src/main/java/module-info.java
@@ -0,0 +1,24 @@
+module jvmxray.shared {
+    // Project Dependencies
+    // None
+
+    // Project External Dependencies
+    requires java.rmi;
+    requires org.slf4j;
+    requires ch.qos.logback.classic;
+    requires ch.qos.logback.core;
+    requires com.datastax.oss.driver.core;
+    requires com.datastax.oss.driver.querybuilder;
+    requires com.fasterxml.jackson.databind;
+
+    // Private Project Exports
+    exports org.jvmxray.platform.shared.classloader to jvmxray.agent, jvmxray.logserver;
+    exports org.jvmxray.platform.shared.event to jvmxray.agent, jvmxray.logserver;
+    exports org.jvmxray.platform.shared.property to jvmxray.agent, jvmxray.logserver;
+    exports org.jvmxray.platform.shared.util to jvmxray.agent, jvmxray.logserver;
+
+    // Public API Exports
+    exports org.jvmxray.platform.shared.logback.appender.cassandra;
+    exports org.jvmxray.platform.shared.logback.codec;
+    exports org.jvmxray.platform.shared.logback.converter;
+}
